567313ad38184d70f2c1cbb3ed3d2ebb0ab834d1
Yawar Hussain <yawarhussain793@gmail.com>
2026-01-19 09:52:29 -0800

    Test the code and fix the bugs and run it

--- Task2.py
+++ Task2.py
@@ -41,12 +41,16 @@ def Stock_Portfolio_Tracker():
         investment +=  companys[stock_name]*quantity
         total_investment += investment 
 
+        Stock_data.append(f"{stock_name} - {quantity} - {investment}")
+        print(f"Stock name: {stock_name}\tQuantity: {quantity}\tInvestment: {investment}")
+        print(f"Overall Investment: {total_investment}")
+
     save = input("Do you want to save stock data into csv file : (yes/no) :> ")
     if save.lower() == 'yes':
         with open('Stockdata.csv','w') as file:
             file.write('Stock\tQuantity\tInvestment')
             for item in companys:
-                file.write(f"{item}+\n")
+                file.write(f"{item}\n")
             file.write(f"Total Investment : {total_investment}")
         print("File save successfully into csv formate ")
 
