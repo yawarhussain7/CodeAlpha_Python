6a0a9d3b8a85f0b5127d3dfde4009d090ef58c67
Yawar Hussain <yawarhussain793@gmail.com>
2026-01-19 09:25:37 -0800

    Task2.py create file for task 2:

---
+++ Task2.py
@@ -0,0 +1,9 @@
+# TASK 2: Stock Portfolio Tracker 
+# ● Goal: Build a simple stock tracker that calculates total investment based on manually defined stock 
+# prices. 
+# ● Simplified Scope: 
+# ○ User inputs stock names and quantity. 
+# ○ Use a hardcoded dictionary to define stock prices (e.g., {"AAPL": 180, "TSLA": 250}). 
+# ○ Display total investment value and optionally save the result in a .txt or .csv file. 
+# ● Key Concepts Used: dictionary, input/output, basic arithmetic, file handling 
+# (optional).
