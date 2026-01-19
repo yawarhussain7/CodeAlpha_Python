d37f95403110e0c5190ab94a00241942e5b2f15f
Yawar Hussain <yawarhussain793@gmail.com>
2026-01-19 04:49:51 -0800

    add the display screen text and using loop show the game details and take input from user

--- Task1.py
+++ Task1.py
@@ -5,9 +5,30 @@ trial_no=0
 guees_words=[]
 
 def Handman():
+    print("<===== Welcome to Handman =====>\n")
     predefine_words=['Python','Java','Cpp','C','Ruby']
     word = random.choice(predefine_words)
-    print(word)
+    
+    # show the hidden txt on the console
+    display_screen=['_']*len(word)
+
+    while trial_no >= guess_limit:
+
+        print("Word: ".join(display_screen))
+        print(f'Trial Number: {trial_no}')
+        print(f'Remaning Limit: {guess_limit}')
+
+        user_input = input("Enter a words").capitalize()
+
+        if len(user_input)!= 1 or user_input.isalpha()==False:
+            print("Invalid Input! Please enter a single alphabetic character.")
+            continue
+        
+
+
+    
+
+    
 
 
 
